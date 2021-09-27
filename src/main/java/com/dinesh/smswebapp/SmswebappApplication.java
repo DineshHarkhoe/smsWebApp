5e3df14e474543de3794cc0dd10d7e3b2e3ebd9c
dinesh <dinharkhoe@gmail.com>
2021-09-26 23:17:02 -0300

    initial commit

---
+++ src/main/java/com/dinesh/smswebapp/SmswebappApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.smswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmswebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmswebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dinesh/smswebapp/SmswebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.smswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmswebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
